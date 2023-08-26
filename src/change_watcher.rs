e03f9b6bb5e34f771843887a588b9a74caed2942
a <b>
2023-08-26 22:13:55 +0200

    full API

---
+++ src/change_watcher.rs
@@ -0,0 +1,127 @@
+use actix_files::NamedFile;
+use actix_web::http::StatusCode;
+use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer, ResponseError, get};
+use actix_web_actors::ws;
+use notify_debouncer_mini::{new_debouncer, DebounceEventResult, Debouncer, DebouncedEvent};
+use std::path::{PathBuf};
+use std::time::{Duration};
+use tokio::sync::mpsc::{self, UnboundedSender};
+use actix::AsyncContext;
+use actix::StreamHandler;
+use notify::{RecursiveMode,  ReadDirectoryChangesWatcher};
+
+impl ResponseError for ImgetError {
+    fn status_code(&self) -> StatusCode {
+        self.status_code
+    }
+    fn error_response(&self) -> HttpResponse {
+        // Customize the HTTP response for your custom error
+        HttpResponse::InternalServerError().body(self.message.clone())
+    }
+}
+
+#[derive(Debug)]
+struct ImgetError {
+    message: String,
+    status_code: StatusCode
+}
+impl std::fmt::Display for ImgetError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
+
+impl std::convert::From<notify::Error> for ImgetError {
+    fn from(err: notify::Error) -> Self {
+        return ImgetError { message: format!("Notify Error: {}", err), status_code: StatusCode::INTERNAL_SERVER_ERROR }
+    }
+}
+
+impl std::error::Error for ImgetError {}
+
+pub fn get_folder_watcher(tx: UnboundedSender<Vec<DebouncedEvent>>) -> Debouncer<ReadDirectoryChangesWatcher> {
+    // Select recommended watcher for debouncer.
+    // Using a callback here, could also be a channel.
+    let debouncer = new_debouncer(Duration::from_millis(500), move |res: DebounceEventResult| {
+        match res {
+            Ok(changes) => { 
+                match tx.send(changes) {
+                    Ok(()) => {
+                        println!("Sent event")
+                    },
+                    Err(e) => {
+                        eprintln!("Error sending file change event: {}", e);
+                    }
+                }
+            },
+            Err(e) => {
+                eprintln!("File change event error: {}", e);
+            }
+        }
+        
+    }).unwrap();
+
+    debouncer
+}
+
+
+#[derive(serde::Serialize)]
+struct FileChange {
+    path: String,
+}
+
+#[derive(serde::Serialize)]
+struct FileChanges {
+    changes: Vec<FileChange>,
+}
+
+pub struct WatcherWsActor {
+    pub rx: mpsc::UnboundedReceiver<Vec<DebouncedEvent>>,
+    pub debouncer: Debouncer<ReadDirectoryChangesWatcher>
+}
+
+impl actix::Actor for WatcherWsActor {
+    type Context = ws::WebsocketContext<Self>;
+
+    fn started(&mut self, ctx: &mut Self::Context) {
+        // Start sending file change events to the WebSocket client
+        ctx.run_interval(Duration::from_secs(1), |act, ctx| {
+            if let Ok(events) = act.rx.try_recv() {
+                let changes_vec: Vec<FileChange> = events.into_iter()
+                    .filter_map(|e| e.path.to_str()
+                    .map(|s| FileChange{ path: String::from(s)})).collect();
+                let changes = FileChanges {changes: changes_vec};
+                let string_data = serde_json::to_string(&changes).unwrap();
+                ctx.text(string_data);
+            }
+        });
+    }
+}
+
+#[derive(serde::Deserialize, serde::Serialize)]
+struct WatchFolder {
+    watch: bool,
+    path: String,
+}
+//{"watch": true, "path": "Q:\\"}
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WatcherWsActor {
+    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+        match msg {
+            Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
+            Ok(ws::Message::Text(msg)) => {
+                let watch_command: WatchFolder = serde_json::from_slice(&msg.as_bytes()).unwrap();
+                if watch_command.watch {
+                    if let Err(e) = self.debouncer.watcher().watch(&PathBuf::from(watch_command.path.clone()), RecursiveMode::NonRecursive) {
+                        println!("Error watching folder ({}): {}", watch_command.path.clone(), e)
+                    }
+                } else {
+                    if let Err(e) = self.debouncer.watcher().unwatch(&PathBuf::from(watch_command.path.clone())) {
+                        println!("Error unwatching folder ({}): {}", watch_command.path, e)
+                    } 
+                }
+                ()
+            },
+            _ => (),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, Error};
+use actix_files::NamedFile;
+use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer, get};
+use actix_web_actors::ws;
+use std::{path::PathBuf, time::{SystemTime, UNIX_EPOCH}};
+use tokio::sync::mpsc;
+
+mod change_watcher;
+
 use serde::{Serialize, Deserialize};
 use std::fs::{self, Metadata};
 
@@ -6,6 +13,7 @@ use std::fs::{self, Metadata};
 struct FileEntry {
     name: String,
     is_directory: bool,
+    last_modified: u128
 }
 
 #[derive(Deserialize)]
@@ -18,13 +26,18 @@ async fn get_files(web::Query(params): web::Query<FileRequestParam>) -> Result<H
     let directory = params.directory;
     let entries = fs::read_dir(directory)?
         .filter_map(|entry| {
-            let entry = entry.unwrap();
-            let metadata = entry.metadata().unwrap();
+            let entry = entry.ok()?;
+            let metadata = entry.metadata().ok()?;
             let is_directory = metadata.is_dir();
-            let name = entry.file_name().into_string().unwrap();
+            let name = entry.file_name().into_string().ok()?;
+            let last_modified = metadata.modified().ok()?
+                .duration_since(UNIX_EPOCH).ok()?
+                .as_millis();
+
             Some(FileEntry {
                 name,
                 is_directory,
+                last_modified
             })
         })
         .collect::<Vec<_>>();
@@ -32,10 +45,30 @@ async fn get_files(web::Query(params): web::Query<FileRequestParam>) -> Result<H
     Ok(HttpResponse::Ok().json(entries))
 }
 
+#[get("/ws/watch")]
+async fn watch(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+    let (tx, rx) = mpsc::unbounded_channel();
+    // Get a debouncer over notify to watch changes
+    let debouncer = change_watcher::get_folder_watcher(tx);
+    // Upgrade the connection to a WebSocket
+    let resp: HttpResponse = ws::start(change_watcher::WatcherWsActor { rx, debouncer }, &req, stream)?;
+    Ok(resp)
+}
+
+#[get("/file/{filename:.*}")]
+async fn static_files(req: HttpRequest) -> Result<NamedFile, Error> {
+    let path: PathBuf = req.match_info().query("filename").parse()?;
+    let file = NamedFile::open(path)?;
+    Ok(file)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().service(get_files)
+        App::new()
+            .service(watch)
+            .service(static_files)
+            .service(get_files)
     })
     .bind("127.0.0.1:8080")?
     .run()
